67ac52b3ce5be28e5fe8d6959ec633f6d70f43eb
Viacheslav <v.konovalov@crystals.ru>
2023-10-27 16:19:23 +0300

    cleanup

--- LoggerXmlGraphViewer.py
+++ LoggerXmlGraphViewer.py
@@ -1,6 +1,12 @@
-﻿import matplotlib.pyplot as plt
+#! /bin/env python3
+
+import math
+import os
 import xml.etree.ElementTree as ET
-import math, os, numpy
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy
 
 
 def get_datetime_from_report(report, datetime):
@@ -16,7 +22,7 @@ def get_float_from_report(report, param):
     if ',' in temp:
         temp = temp.replace(',', '.')
     return (float(temp))
-    
+
 def set_plt_title_and_ylabel(plt, title, label):
     plt.title(title, fontsize = 14)
     plt.ylabel(label, fontsize = 12)
@@ -27,7 +33,7 @@ def get_acc_setting(report, param):
     try:
         return settings.find(param)
     except:
-        return    
+        return
 
 def get_acc_time(report):
     header = report.find('header')
@@ -38,23 +44,22 @@ def get_acc_time(report):
 
 def get_list_files(file_name):
     # Set path to data file
-    current_path = os.path.dirname(os.path.realpath(__file__))
-    data_path = current_path + '\\data'
-    dir_list = os.listdir(data_path)
-    file_list = os.listdir(data_path + '\\' + dir_list[0])
+    current_path =Path(__file__).parent.resolve().joinpath('data')
+    dir_list = [d for d in current_path.iterdir() if d.is_dir()]
+    file_list =[f for f in Path(dir_list[0]).iterdir() if f.is_file()]
 
     file_list.sort()
     idx = 0
     for (i, file) in enumerate(file_list):
-        if (file_name in file):
-            print ('file: ' + file)
+        if (file_name in file.name):
+            print (f'file: {file}')
             idx = i
             break
     print(idx)
     del file_list[0:idx]
     return file_list
-    
-def show_tph_graph(path, type = 'Temperature'):
+
+def show_tph_graph(path: Path, type = 'Temperature'):
     tree = ET.parse(path)
     reports = tree.getroot()
     if reports.tag != 'tph_report':
@@ -65,13 +70,13 @@ def show_tph_graph(path, type = 'Temperature'):
     hdc_hum = [] # hummidity from hdc1080
     lps_temp = [] # temperature from lps331
     lps_p = [] # pressure from lps331
-    
+
     for report in reports:
         # Get node timestamp for x_ticks
         x_tick.append(get_datetime_from_report(report, 'time')[:-4])
         # Get data from hdc1080 node
         hdc_temp.append(get_float_from_dev_report(report, 'hdc1080', 't'))
-        hdc_hum.append(get_float_from_dev_report(report, 'hdc1080', 'hum'))        
+        hdc_hum.append(get_float_from_dev_report(report, 'hdc1080', 'hum'))
         # Get data from lps331 node
         lps_temp.append(get_float_from_dev_report(report, 'lps331', 't'))
         lps_p.append(get_float_from_dev_report(report, 'lps331', 'p'))
@@ -80,7 +85,7 @@ def show_tph_graph(path, type = 'Temperature'):
     fig, ax = plt.subplots()
     plt.xlabel("Time", fontsize = 8)
     # Generate a list from 0 to report number
-    x = [++i for i in range(len(reports))]
+    x = [i+1 for i in range(len(reports))]
     if type == 'Humidity':
         set_plt_title_and_ylabel(plt,'Humidity graphic', 'Humidity')
         ax.plot(x, hdc_hum, 'b-', label='hdc1080 humidity')
@@ -94,7 +99,7 @@ def show_tph_graph(path, type = 'Temperature'):
         ax.plot(x, hdc_temp, 'r-', label='hdc_temp')
         ax.plot(x, lps_temp, 'y-', label='lps331')
         y_mean = numpy.mean(hdc_temp)
-        
+
     # Create x_tick, grid and legend
     plt.xticks(x, x_tick, rotation = 90)
     plt.grid(linestyle = 'dashed')
@@ -106,11 +111,11 @@ def show_tph_graph(path, type = 'Temperature'):
 
     # Set fig to fullscreen
     mng = plt.get_current_fig_manager()
-    mng.window.state('zoomed')
+    # mng.window.state('zoomed')
 
     plt.show()
 
-def show_acc_graph(path):
+def show_acc_graph(path:Path):
     tree = ET.parse(path)
     root = tree.getroot()
     if root.tag != 'acc_report':
@@ -120,7 +125,7 @@ def show_acc_graph(path):
     accx = [] # acc for axis
     accy = [] # acc for axis
     accz = [] # acc for axis
-    
+
     # get node data
     freq = float(get_acc_setting(root, 'freq').text)
     reports = root.find('report')
@@ -129,23 +134,23 @@ def show_acc_graph(path):
         accx.append(get_float_from_report(report, 'x'))
         accy.append(get_float_from_report(report, 'y'))
         accz.append(get_float_from_report(report, 'z'))
-    
+
     # Create plots
     fig, ax = plt.subplots()
     set_plt_title_and_ylabel(plt, 'ACC graphic', 'g')
     plt.xlabel("Time", fontsize = 8)
 
     # Generate a list from 0 to report number
-    x = [++i for i in range(len(reports))]
+    x = [i+1 for i in range(len(reports))]
     ax.plot(x, accx, 'r-', label='x')
     ax.plot(x, accy, 'y-', label='y')
     ax.plot(x, accz, 'g-', label='z')
-    
+
     # Create x_tick, grid and legend
     x_tick[0] = get_acc_time(root)
-    plt.xticks(x, x_tick, rotation = 90)    
+    plt.xticks(x, x_tick, rotation = 90)
     plt.grid(linestyle = 'dashed')
-    ax.legend(loc='best')    
+    ax.legend(loc='best')
 
     # Set the origin
     ax.axhline(y = 0, color = 'k')
@@ -153,11 +158,11 @@ def show_acc_graph(path):
 
     # Set fig to fullscreen
     mng = plt.get_current_fig_manager()
-    mng.window.state('zoomed')
+    # mng.window.state('zoomed')
 
     plt.show()
 
-def show_jrnl_graph(path, type = 'Voltage'):
+def show_jrnl_graph(path: Path, type = 'Voltage'):
     tree = ET.parse(path)
     journal = tree.getroot()
     if journal.tag != 'jornal':
@@ -166,13 +171,13 @@ def show_jrnl_graph(path, type = 'Voltage'):
     accx = [] # acc for axis
     accy = [] # acc for axis
     accz = [] # acc for axis
-    
+
     x_tick = [] # x ticks like 10:42:30:513, 10:42:31:513 and etc
     hdc_temp = [] # temperature from hdc1080
     hdc_hum = [] # hummidity from hdc1080
     lps_temp = [] # temperature from lps331
     lps_p = [] # pressure from lps331
-    
+
     volt = [] # voltage
 
     for record in journal:
@@ -180,7 +185,7 @@ def show_jrnl_graph(path, type = 'Voltage'):
         x_tick.append(get_datetime_from_report(record, 'time')[:-4])
         # Get data from hdc1080 node
         hdc_temp.append(get_float_from_dev_report(record, 'hdc1080', 't'))
-        hdc_hum.append(get_float_from_dev_report(record, 'hdc1080', 'hum'))        
+        hdc_hum.append(get_float_from_dev_report(record, 'hdc1080', 'hum'))
         # Get data from lps331 node
         lps_temp.append(get_float_from_dev_report(record, 'lps331', 't'))
         lps_p.append(get_float_from_dev_report(record, 'lps331', 'p'))
@@ -191,43 +196,43 @@ def show_jrnl_graph(path, type = 'Voltage'):
         accx.append(get_float_from_report(acc_record, 'x'))
         accy.append(get_float_from_report(acc_record, 'y'))
         accz.append(get_float_from_report(acc_record, 'z'))
-    
+
     # Create plots
     fig, ax = plt.subplots()
     plt.xlabel("Time", fontsize = 8)
     # Generate a list from 0 to report number
-    x = [++i for i in range(len(journal))]
+    x = [i+1 for i in range(len(journal))]
     y_mean = 0
     if type == 'Humidity':
         set_plt_title_and_ylabel(plt, 'Humidity graphic', 'Humidity')
         ax.plot(x, hdc_hum, 'b-', label = 'hdc1080 humidity')
         y_mean = numpy.mean(hdc_hum)
-        
+
     elif type == 'Pressure':
         set_plt_title_and_ylabel(plt, 'Pressure graphic', 'Pressure')
         ax.plot(x, lps_p, 'r-', label = 'lps331 pressure')
         y_mean = numpy.mean(lps_p)
-        
+
     elif type == 'Temperature':
         set_plt_title_and_ylabel(plt, 'Temperature graphic', 'Temperature')
         ax.plot(x, hdc_temp, 'r-', label = 'hdc_temp')
         ax.plot(x, lps_temp, 'y-', label = 'lps331')
         y_mean = numpy.mean(hdc_temp)
-        
+
     elif type == 'Voltage':
         set_plt_title_and_ylabel(plt, 'Battery voltage graphic', 'V')
         ax.plot(x, volt, 'b-', label = 'Voltage')
         y_mean = numpy.mean(volt)
-        
+
     elif type == 'Acc':
         set_plt_title_and_ylabel(plt, 'ACC graphic', 'g')
         ax.plot(x, accx, 'r-', label='x')
         ax.plot(x, accy, 'y-', label='y')
         ax.plot(x, accz, 'g-', label='z')
-        
+
     else:
         return
-        
+
     # Create x_tick, grid and legend
     plt.xticks(x, x_tick, rotation = 90)
     plt.grid(linestyle = 'dashed')
@@ -243,33 +248,33 @@ def show_jrnl_graph(path, type = 'Voltage'):
 
     # Set fig to fullscreen
     mng = plt.get_current_fig_manager()
-    mng.window.state('zoomed')
+    # mng.window.state('zoomed')
 
     plt.show()
-    
-def show_graph(path):
-    if not os.path.isfile(path):
+
+def show_graph(path:Path):
+    if not path.is_file():
         return
-    if 'acc_report' in path:
+    if 'acc_report' in path.name:
         show_acc_graph(path)
-    if 'tph_report' in path:
+    if 'tph_report' in path.name:
         show_tph_graph(path)
-    if 'jornal' in path:
+    if 'jornal' in path.name:
         show_jrnl_graph(path)
 
 
 def main():
-    # fl = get_list_files('tph_report')
-    fl = get_list_files('acc_report')
+    fl = get_list_files('tph_report')
+    # fl = get_list_files('acc_report')
     # fl = get_list_files('jornal')
     #print(fl)
-    current_path = os.path.dirname(os.path.realpath(__file__))
-    data_path = current_path + '\\data'
-    dir_list = os.listdir(data_path)
-    path = data_path + '\\' + dir_list[0] + '\\' + fl[0]
+    current_path = Path(__file__).parent.resolve()
+    data_path = current_path.joinpath('data')
+    dir_list = [d for d in current_path.iterdir() if d.is_dir()]
+    path = data_path.joinpath(dir_list[0]).joinpath(fl[0])
     print(path)
     show_graph(path)
-    
+
 
 if __name__ == '__main__':
     main()
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # logger_xml_graph
 Python script for visualistion data logs
+
+## Issue: UserWarning: FigureCanvasAgg is non-interactive, and thus cannot be shown
+
+```bash
+# Then need install
+sudo apt-get install python3-tk
+# or
+pip install pyqt5
+```
